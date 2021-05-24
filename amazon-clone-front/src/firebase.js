9c83df2e7dfcd8e537228cf45932232476357274
Harsh tenguriya <thegreatharsh1@gmail.com>
2021-05-24 22:17:50 +0530

    bolier plate

---
+++ amazon-clone-front/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBXYFLl8lV1zxRAC60f9FLSw8YhxZYjfnQ",
+  authDomain: "clone-56378.firebaseapp.com",
+  projectId: "clone-56378",
+  storageBucket: "clone-56378.appspot.com",
+  messagingSenderId: "139062605688",
+  appId: "1:139062605688:web:7822e2665b555985aeba8d",
+  measurementId: "G-BSDXHB36VV",
+};
